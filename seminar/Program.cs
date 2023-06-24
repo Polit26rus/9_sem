8901a94a9abb89be6a8c09fd4c7c4f30f30bafa2
polit <iluhapolitov@gmail.com>
2023-06-24 15:09:31 +0300

    a

---
+++ seminar/Program.cs
@@ -0,0 +1,35 @@
+﻿// 65: Задайте значения М и Н,напишите программу,которая выведет все натуральные
+// числа в промежутке от M до N
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+// 65
+// string PrintNumbers(int start, int finish)
+// {
+//     if (start == finish)
+//     {
+//         return start.ToString();
+//     }
+//     return start + " " + PrintNumbers(start + 1,finish); 
+// }
+// Console.WriteLine(PrintNumbers(m, n));
+
+int SumOfNum(int n){
+    int sum = 0;
+    if(n == 0){
+        return sum;
+    }
+    else{
+        return sum += n % 10;
+        n /= n / 10;
+    }
+}
+
+
+int m = Prompt("Введите m ");
+int n = Prompt("Введите n ");
