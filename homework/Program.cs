d2f8e57de0a5b93facc52f8b794352a17755c88c
polit <iluhapolitov@gmail.com>
2023-06-24 15:30:25 +0300

    64

---
+++ homework/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// 64
+// string PrintNumbers(int finish)
+// {
+//     if (finish == 1)
+//     {
+//         return "1";
+//     }
+//     return finish + " " + PrintNumbers(finish - 1); 
+// }
+
+
+int n = Prompt("Введите n ");
+
+// 64
+// Console.WriteLine(PrintNumbers(n));
